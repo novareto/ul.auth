ee11b8512328850e6a16faa8b90fd3fec6fe5d02
Souheil CHELFOUH <trollfot@gmail.com>
2014-10-10 21:59:16 +0200

    Updated login form

--- src/ul/auth/browser.py
+++ src/ul/auth/browser.py
@@ -1,24 +1,79 @@
 # -*- coding: utf-8 -*-
 
-from os import path
-from .app import AuthMiddleware
-from dolmen.template import TALTemplate
-from grokcore.component import context, name
-from ul.browser import Page
+from ul.browser.components import Form, Fields, action
+from grokcore.component import name, context
+from . import require, UserLoggedInEvent, Principal
+
+from cromlech.browser import exceptions, getSession
+from dolmen.forms.base import FAILURE, SuccessMarker
+from zope.component import getUtility
+from zope.component.hooks import getSite
+from zope.event import notify
+from zope.interface import Interface
+from zope.schema import TextLine, Password
 
 
-class Login(Page):
-    context(AuthMiddleware)
+class ICredentials(Interface):
+
+    def assert_credentials(username, password):
+        """Returns a boolean
+        """
+
+
+class ILoginForm(Interface):
+
+    username = TextLine(
+        title=u'Username',
+        required=True)
+
+    password = Password(
+        title=u"Password",
+        required=True)
+
+
+class Login(Form):
     name('login')
-    template = TALTemplate(path.join(path.dirname(__file__), 'login.cpt'))
+    context(Interface)
+    require('zope.Public')
 
-    title = message = u"Please log in"
-    action = ""
+    fields = Fields(ILoginForm)
 
-    def update(self):
-        self.username = self.request.environment.get(
-            self.context.environ_user_key, '')
+    def make_principal(self, *args):
+        return Principal(*args)
+    
+    @action(u'Login')
+    def log_me(self):
+        data, errors = self.extractData()
+        if errors:
+            self.submissionError = errors
+            return FAILURE
 
-        self.userfield = self.context.user_field
-        self.pwdfield = self.context.pass_field
-        self.button = self.context.button
+        # credentials here
+        site = getSite()
+        if site is None:
+            self.flash(u"You can't login here.")
+            return SuccessMarker(
+                'Login failed', False, url=self.url(self.context), code=302)
+
+        credentials = getattr(site, 'credentials', None)
+        if not credentials:
+            self.flash(u"Missing credentials.")
+            return SuccessMarker(
+                'Login failed', False, url=self.url(self.context), code=302)
+
+        for credential in credentials:
+            credentials_manager = getUtility(ICredentials, name=credential)
+            account = credentials_manager.log_in(**data)
+            if account:
+                session = getSession()
+                session['username'] = data['username']
+                self.flash(u"Login successful.")
+                principal = make_principal(data['username'])
+                self.request.principal = principal
+                notify(UserLoggedInEvent(principal))
+                return SuccessMarker(
+                    'Login successful', True, url=self.url(self.context),
+                    code=302)
+
+        self.flash(u'Login failed.')
+        return FAILURE
